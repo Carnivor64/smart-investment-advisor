9023b2a8ceaf2fd5be16bf601365378c83443584
Carnivor64 <akandil02@gmail.com>
2026-09-25 22:23:44 +0300

    Update smart_investment_advisor.py

--- smart_investment_advisor.py
+++ smart_investment_advisor.py
@@ -51,7 +51,7 @@ st.set_page_config(page_title="Smart Investment Advisor", page_icon="📈", layo
 # جداول users وwatchlist يجب إنشاؤها مسبقاً في مشروع Supabase (انظر README.md
 # لنص SQL الجاهز)، وربط بيانات الاتصال عبر Secrets كما هو موضح في README.md.
 @st.cache_resource
-from supabase import create_client, Client
+from supabase import create_client, client
 from supabase.lib.client_options import ClientOptions
 import streamlit as st
 
